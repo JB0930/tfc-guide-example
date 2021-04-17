6382504528631a9c05782525c32a7f9b352b0021
JB0930 <75677034+JB0930@users.noreply.github.com>
2021-04-17 10:52:42 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
   type    = string
-  default = "us-west-1"
+  default = "us-east-1"
 }
 
 variable "db_table_name" {
@@ -15,5 +15,5 @@ variable "db_read_capacity" {
 
 variable "db_write_capacity" {
   type    = number
-  default = 1
+  default = 2
 }
